5b2037cb773faa3cd8aa35a93ef3dd1017c80877
Raz <raz_st@hotmail.com>
2022-01-18 14:02:31 +0200

    udpate

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print("test ABC45")
+import pygame
+import time
 
+# screen size 
+WINDOW_W = 250
+WINDOW_H = 164
+WINDOW_SIZE = (WINDOW_W, WINDOW_H)
+
+BK_COLOR = (68,132,88)
+
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("My First Game")
+
+bk_image = pygame.image.load("background.jpg")
+
+play = True
+while play:
+  # screen.fill(BK_COLOR)
+  screen.blit(bk_image,(0,0))
+#   pygame.draw.line(screen,(255,255,255),[0,100],[WINDOW_W,100],2)
+  pygame.display.flip()
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      play = False
+
+pygame.quit()
