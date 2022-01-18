af4e2d8c1e65d36bf58bc0fc171df34d0f9c29c3
Raz <raz_st@hotmail.com>
2022-01-18 15:07:34 +0200

    added circles...

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from turtle import circle
 import pygame
 import time
 
 # screen size 
-WINDOW_W = 250
-WINDOW_H = 164
+WINDOW_W = 1000
+WINDOW_H = 800
 WINDOW_SIZE = (WINDOW_W, WINDOW_H)
 
 BK_COLOR = (68,132,88)
@@ -14,14 +15,27 @@ pygame.display.set_caption("My First Game")
 
 bk_image = pygame.image.load("background.jpg")
 
+clock = pygame.time.Clock()
+
+circle_x = 10
+circle_y = WINDOW_H /2
+x_step = 10
 play = True
 while play:
   # screen.fill(BK_COLOR)
   screen.blit(bk_image,(0,0))
-#   pygame.draw.line(screen,(255,255,255),[0,100],[WINDOW_W,100],2)
+  pygame.draw.circle(screen,(255,255,255),(circle_x , circle_y),10)
+  
+  circle_x +=x_step
+  if circle_x > WINDOW_W:
+    x_step = -10
+  if circle_x <0 :
+    x_step = 10
+  
   pygame.display.flip()
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       play = False
+  clock.tick(50)
 
 pygame.quit()
