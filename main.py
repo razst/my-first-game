6d19e72d3f1642fa5bc5b88ef9b4485a13604ca1
Raz <na@users.noreply.github.com>
2022-01-19 14:39:36 +0200

    added ship

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("My First Game")
 
 bk_image = pygame.image.load("background.jpg")
+ship_image = pygame.image.load("ship.png")
+ship_image = pygame.transform.scale(ship_image, (50, 80)) 
 
 clock = pygame.time.Clock()
 
@@ -21,9 +23,21 @@ circle_x = 10
 circle_y = WINDOW_H /2
 x_step = 10
 play = True
+ship_x = WINDOW_W /2
+ship_y = WINDOW_H - 80
 while play:
   # screen.fill(BK_COLOR)
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      play = False
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+          ship_x -= 10
+      if event.key == pygame.K_RIGHT:
+          ship_x += 10
+
   screen.blit(bk_image,(0,0))
+  screen.blit(ship_image,(ship_x,ship_y))
   pygame.draw.circle(screen,(255,255,255),(circle_x , circle_y),10)
   
   circle_x +=x_step
@@ -33,9 +47,8 @@ while play:
     x_step = 10
   
   pygame.display.flip()
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      play = False
-  clock.tick(50)
+
+
+  clock.tick(10)
 
 pygame.quit()
