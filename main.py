23d3c68333d0e5c1216f263d1f5cf16929853091
Raz <na@users.noreply.github.com>
2022-01-25 15:14:28 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import circle
 import pygame
 import time
+import sys,os
 
 # screen size 
 WINDOW_W = 1000
@@ -13,9 +14,12 @@ pygame.init()
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("My First Game")
 
+# www.pngaaa.com
 bk_image = pygame.image.load("background.jpg")
 ship_image = pygame.image.load("ship.png")
 ship_image = pygame.transform.scale(ship_image, (50, 80)) 
+laser_image = pygame.image.load("laser2.png")
+laser_image = pygame.transform.scale(laser_image, (10, 20)) 
 
 clock = pygame.time.Clock()
 
@@ -25,21 +29,51 @@ x_step = 10
 play = True
 ship_x = WINDOW_W /2
 ship_y = WINDOW_H - 80
+laser_list = []
+
+def get_dir_list():
+  return next(os.walk('.\games'))[1]
+
+list_of_games = get_dir_list()
+
+for game in list_of_games:
+  print("the game-",game)
+
+
+
+
+
+
+def print_lasers():
+  for i in range(len(laser_list)):
+    l = laser_list[i]
+    screen.blit(laser_image,(l[0],l[1]))
+    laser_list[i] = [l[0],l[1]-20]
+
+  if len(laser_list) > 0 and laser_list[0][1] < 0:
+    laser_list.remove(laser_list[0])
+
+
+
+
 while play:
   # screen.fill(BK_COLOR)
+  screen.blit(bk_image,(0,0))
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       play = False
     elif event.type == pygame.KEYDOWN:
       if event.key == pygame.K_LEFT:
-          ship_x -= 10
+        ship_x -= 10
       if event.key == pygame.K_RIGHT:
-          ship_x += 10
+        ship_x += 10
+      if event.key == pygame.K_SPACE:
+        laser_list.append([int(ship_x+21),ship_y])
 
-  screen.blit(bk_image,(0,0))
   screen.blit(ship_image,(ship_x,ship_y))
   pygame.draw.circle(screen,(255,255,255),(circle_x , circle_y),10)
-  
+  print_lasers()
+
   circle_x +=x_step
   if circle_x > WINDOW_W:
     x_step = -10
